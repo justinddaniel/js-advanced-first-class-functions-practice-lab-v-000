ec83104f8c70505213015caf1cbd930256cb33cd
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 00:25:03 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // Code your solution in this file!
 function logDriverNames (array) {
-  console.log(array.forEach(function (element) {return element;}));
+  console.log(array.forEach(function (element) {return element.name;}));
 }
