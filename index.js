80bec13618d296b52bf954d214ad27062723fe54
Justin Daniel <justinddaniel@gmail.com>
2018-01-18 00:30:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,3 +2,7 @@
 function logDriverNames (array) {
   array.forEach(function (element) {console.log(element.name);});
 }
+
+function logDriversByHometown (array, string) {
+  array.forEach(function (element) {element.hometown === string ? console.log(element.hometown) : console.log('no match'); });
+}
